f0eb5ec6385dbb2f9c2e87cbefc8b89ca282ee1f
SanntAlv <saalvarez@fi.uba.ar>
2024-06-11 18:35:32 -0300

    agregado de la funcion insertarLista para una lista generica

--- pintureria.c
+++ pintureria.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct snodo{
+	int valor;
+	struct snodo *siguiente;
+}tnodo;
+
+typedef tnodo *tpuntero;
+
+void insertarLista (tpuntero *cabeza, int e){
+	tpuntero nuevo;
+	nuevo=malloc(sizeof(tnodo));
+	nuevo->valor=e;
+	nuevo->siguiente=*cabeza;
+	*cabeza=nuevo;
+}
+
+
+
 int main(){
 
     printf("Hola Mundo\n");
